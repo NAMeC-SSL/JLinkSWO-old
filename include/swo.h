b7c5f5bf2ea7e4262ba514d00653973024a1e28f
Etienne Schmitz <contact@etienne-schmitz.com>
2023-03-19 13:37:48 +0100

    chore: remove namespace

--- include/swo.h
+++ include/swo.h
@@ -3,27 +3,22 @@
 #include <mbed.h>
 #include <Stream.h>
 
-namespace swo
+class SWO : public Stream
 {
+public:
+    void print(const char *s);
+    void print(char c);
+    void print(int n);
+    void print(bool value);
+    void print(double value, int digits = 2);
 
-    class SWO : public Stream
-    {
-    public:
-        void print(const char *s);
-        void print(char c);
-        void print(int n);
-        void print(bool value);
-        void print(double value, int digits = 2);
+    void println(const char *s);
+    void println(char c);
+    void println(int n);
+    void println(bool value);
+    void println(double number, int digits = 2);
 
-        void println(const char *s);
-        void println(char c);
-        void println(int n);
-        void println(bool value);
-        void println(double number, int digits = 2);
-
-    protected:
-        int _putc(int c);
-        int _getc();
-    };
-
-} // namespace swo
+protected:
+    int _putc(int c);
+    int _getc();
+};
--- src/swo.cpp
+++ src/swo.cpp
@@ -1,127 +1,122 @@
 #include "swo.h"
 
-namespace swo
+int SWO::_putc(int c)
 {
+    ITM_SendChar(c);
+    return c;
+}
 
-    int SWO::_putc(int c)
+int SWO::_getc()
+{
+    return -1;
+}
+
+void SWO::print(const char *s)
+{
+    printf("%s", s);
+}
+
+void SWO::print(char c)
+{
+    printf("%c", c);
+}
+
+void SWO::print(int n)
+{
+    printf("%d", n);
+}
+
+void SWO::print(bool value)
+{
+    if (value)
     {
-        ITM_SendChar(c);
-        return c;
+        print("True");
+    }
+    else
+    {
+        print("False");
+    }
+}
+
+void SWO::print(double number, int digits)
+{
+    if (number < 0.0)
+    {
+        print('-');
+        number = -number;
     }
 
-    int SWO::_getc()
+    int int_part = (int)number;
+    double remainder = number - int_part;
+
+    print(int_part);
+
+    if (digits > 0)
     {
-        return -1;
+        print('.');
     }
 
-    void SWO::print(const char *s)
+    while (digits-- > 0)
     {
-        printf("%s", s);
+        remainder *= 10.0;
+        int to_print = (int)remainder;
+        print(to_print);
+        remainder -= to_print;
+    }
+}
+
+void SWO::println(const char *s)
+{
+    printf("%s\n", s);
+}
+
+void SWO::println(char c)
+{
+    printf("%c\n", c);
+}
+
+void SWO::println(int n)
+{
+    printf("%d\n", n);
+}
+
+void SWO::println(bool value)
+{
+    if (value)
+    {
+        println("True");
+    }
+    else
+    {
+        println("False");
+    }
+}
+
+void SWO::println(double number, int digits)
+{
+    if (number < 0.0)
+    {
+        print('-');
+        number = -number;
     }
 
-    void SWO::print(char c)
+    int int_part = (int)number;
+    double remainder = number - int_part;
+
+    print(int_part);
+
+    if (digits > 0)
     {
-        printf("%c", c);
+        print('.');
     }
 
-    void SWO::print(int n)
+    while (digits-- > 0)
     {
-        printf("%d", n);
+        remainder *= 10.0;
+        int to_print = (int)remainder;
+        print(to_print);
+        remainder -= to_print;
     }
 
-    void SWO::print(bool value)
-    {
-        if (value)
-        {
-            print("True");
-        }
-        else
-        {
-            print("False");
-        }
-    }
-
-    void SWO::print(double number, int digits)
-    {
-        if (number < 0.0)
-        {
-            print('-');
-            number = -number;
-        }
-
-        int int_part = (int)number;
-        double remainder = number - int_part;
-
-        print(int_part);
-
-        if (digits > 0)
-        {
-            print('.');
-        }
-
-        while (digits-- > 0)
-        {
-            remainder *= 10.0;
-            int to_print = (int)remainder;
-            print(to_print);
-            remainder -= to_print;
-        }
-    }
-
-    void SWO::println(const char *s)
-    {
-        printf("%s\n", s);
-    }
-
-    void SWO::println(char c)
-    {
-        printf("%c\n", c);
-    }
-
-    void SWO::println(int n)
-    {
-        printf("%d\n", n);
-    }
-
-    void SWO::println(bool value)
-    {
-        if (value)
-        {
-            println("True");
-        }
-        else
-        {
-            println("False");
-        }
-    }
-
-    void SWO::println(double number, int digits)
-    {
-        if (number < 0.0)
-        {
-            print('-');
-            number = -number;
-        }
-
-        int int_part = (int)number;
-        double remainder = number - int_part;
-
-        print(int_part);
-
-        if (digits > 0)
-        {
-            print('.');
-        }
-
-        while (digits-- > 0)
-        {
-            remainder *= 10.0;
-            int to_print = (int)remainder;
-            print(to_print);
-            remainder -= to_print;
-        }
-
-        print("\n");
-    }
-
-} // namespace swo
+    print("\n");
+}
